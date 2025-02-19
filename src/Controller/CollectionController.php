529277e8ea705e22688154732c99a5d109f768ed
Slaaaayz <maxime.chort@ynov.com>
2025-02-19 15:19:00 +0100

    picture watch in api

--- src/Controller/CollectionController.php
+++ src/Controller/CollectionController.php
@@ -2,16 +2,20 @@
 
 namespace App\Controller;
 
+use App\Repository\WatchRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-final class CollectionController extends AbstractController{
+final class CollectionController extends AbstractController
+{
     #[Route('/collection', name: 'app_collection')]
-    public function index(): Response
+    public function index(WatchRepository $watchRepository): Response
     {
+        $watches = $watchRepository->findAll();
+
         return $this->render('collection/index.html.twig', [
-            'controller_name' => 'CollectionController',
+            'watches' => $watches,
         ]);
     }
 }
