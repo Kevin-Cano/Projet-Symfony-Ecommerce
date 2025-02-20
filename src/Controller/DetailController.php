bd614610fdd8546b1d1952c2f2059578f72a5750
Slaaaayz <maxime.chort@ynov.com>
2025-02-20 09:25:26 +0100

    detailController + picture watch in api

--- src/Controller/DetailController.php
+++ src/Controller/DetailController.php
@@ -2,16 +2,18 @@
 
 namespace App\Controller;
 
+use App\Entity\Watch;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-final class DetailController extends AbstractController{
-    #[Route('/detail', name: 'app_detail')]
-    public function index(): Response
+final class DetailController extends AbstractController
+{
+    #[Route('/detail/{id}', name: 'app_watch_details')]
+    public function index(Watch $watch): Response
     {
-        return $this->render('detail/index.html.twig', [
-            'controller_name' => 'DetailController',
+        return $this->render('collection/details.html.twig', [
+            'watch' => $watch,
         ]);
     }
 }
